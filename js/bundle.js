d45faeb36ffb4c56cea23ac917079bb50701e4b6
amadu <ndongamadou@gmail.com>
2022-01-31 13:14:44 +0100

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -53,6 +53,11 @@ function updatePane(data, title){
     $('#orgsCFM').text(arrOrgs.length);
 }
 
+function slugify(texte){
+    return texte.toLowerCase()
+             .replace(/[^\w ]+/g, '')
+             .replace(/ +/g, '-');
+}
 //return the unique values of given col name
 function getColumnUniqueValues(){
     var values = [];
@@ -745,6 +750,23 @@ function getFirstCountryOfRegion(region){
   region == 'EURO' ? country = 'TUR' : '';
   return country;
 } //getFirstCountryOfRegion
+// get status formatted in tags
+function getFormattedStatus(item){
+    var items = [] ;
+    var arr = item.split(",");
+    var trimedArr = arr.map(x => x.trim());
+    for (let index = 0; index < trimedArr.length; index++) { //remove empty elements
+        if (trimedArr[index]) {
+            items.push(trimedArr[index]);
+        }
+    }
+    var formatedDims = "";
+    items.forEach(element => {
+        var className = slugify(element);
+        formatedDims +='<label class="alert tag-'+className+'">'+element+'</label>';
+    });
+    return formatedDims;
+}//getFormattedStatus
 
 function getDataTableData(data = filteredCfmData){
     var dt = [];
@@ -752,9 +774,9 @@ function getDataTableData(data = filteredCfmData){
         dt.push(
             [
                 element['id'],
-                element['Country'],
-            // getFormattedDimension(element['dimension']), 
                 element['Organisation Name'],
+                element['Country'],
+                getFormattedStatus(element['Status']), 
                 element['Frequency'], 
                 element['Channels'],
                 element['Emergency'], 
@@ -781,6 +803,7 @@ function generateDataTable(){
                 "defaultContent": '<i class="fa fa-plus-circle"></i>',
                 "width": "1%"
             },
+            {"width": "10%"},
             {"width": "15%"},
             {"width": "10%"},
             {"width": "5%"},
@@ -797,14 +820,14 @@ function generateDataTable(){
                 "defaultContent": "-",
                 "targets": "_all"
             },
-            {"targets": [7], "visible": false},{"targets": [8], "visible": false},{"targets": [9], "visible": false},
-            {"targets": [10], "visible": false},{"targets": [11], "visible": false},{"targets": [12], "visible": false},
-            {"targets": [13], "visible": false},{"targets": [14], "visible": false},{"targets": [15], "visible": false},
-            {"targets": [16], "visible": false},{"targets": [16], "visible": false}
+            {"targets": [8], "visible": false},{"targets": [9], "visible": false},{"targets": [10], "visible": false},
+            {"targets": [11], "visible": false},{"targets": [12], "visible": false},{"targets": [13], "visible": false},
+            {"targets": [14], "visible": false},{"targets": [15], "visible": false},{"targets": [16], "visible": false},
+            {"targets": [18], "visible": false},{"targets": [17], "visible": false}
             // { "searchable" : true, "targets": "_all"},
             // {"type": "myDate","targets": 4}
         ],
-        "pageLength": 10,
+        "pageLength": 20,
         "bLengthChange": false,
         "pagingType": "simple_numbers",
         "order":[[1, 'asc']],
@@ -819,8 +842,8 @@ function generateDataTable(){
                         format:{
                             header: function(data, columnIdx){
                                 var hd = ['Name', 'Scale', '# Feedbacks (last 6 months)', 'Target', 'Details','Keyword','National Coordination','Inter-agency','Partners', 'Contact email', 'Status'];
-                                if(columnIdx >= 7){
-                                    return hd[columnIdx-7];
+                                if(columnIdx >= 8){
+                                    return hd[columnIdx-8];
                                 }else {
                                     return data;
                                 }
@@ -858,6 +881,7 @@ function generateDataTable(){
 
 $("#exportTable").on("click", function() {
     // datatable.button( '.buttons-excel' ).trigger();
+    console.log("downloading...")
     $(".buttons-excel").trigger("click");
 });
 
@@ -873,35 +897,35 @@ function format(arr){
                             '<tbody>'+
                                 '<tr>'+
                                     '<td><strong>Name</strong></td>'+
-                                    '<td>'+filtered[0]['Name']+'</td>'+
+                                    '<td colspan="2">'+filtered[0]['Name']+'</td>'+
                                     '<td><strong>Start date</strong></td>'+
-                                    '<td>'+filtered[0]['Start date']+'</td>'+
+                                    '<td colspan="2">'+filtered[0]['Start date']+'</td>'+
                                     '<td><strong># Feedback</strong></td>'+
-                                    '<td>'+filtered[0]['# Feedbacks (last 6 months)']+'</td>'+
+                                    '<td colspan="2">'+filtered[0]['# Feedbacks (last 6 months)']+'</td>'+
                                 '</tr>'+
                                 '<tr>'+
                                     '<td><strong>Scale</strong></td>'+
-                                    '<td>'+filtered[0]['Scale']+'</td>'+
+                                    '<td colspan="2">'+filtered[0]['Scale']+'</td>'+
                                     '<td><strong>National Coordination<strong></td>'+
-                                    '<td>'+filtered[0]['National Coordination']+'</td>'+
+                                    '<td colspan="2">'+filtered[0]['National Coordination']+'</td>'+
                                     '<td><strong>Partners<strong></td>'+
-                                    '<td>'+filtered[0]['Partners']+'</td>'+
+                                    '<td colspan="2">'+filtered[0]['Partners']+'</td>'+
                                 '</tr>'+
                                 '<tr>'+
                                     '<td><strong>Status<s/trong></td>'+
-                                    '<td>'+filtered[0]['Status']+'</td>'+
+                                    '<td colspan="2">'+filtered[0]['Status']+'</td>'+
                                     '<td><strong>Interagency</strong></td>'+
-                                    '<td>'+filtered[0]['Inter-agency']+'</td>'+
+                                    '<td colspan="2">'+filtered[0]['Inter-agency']+'</td>'+
                                     '<td><strong>Keywords</strong></td>'+
-                                    '<td>'+filtered[0]['Keyword']+'</td>'+
+                                    '<td colspan="2">'+filtered[0]['Keyword']+'</td>'+
                                 '</tr>'+
                                 '<tr>'+
                                     '<td><strong>Target</strong></td>'+
-                                    '<td>'+filtered[0]['Target']+'</td>'+
+                                    '<td colspan="2">'+filtered[0]['Target']+'</td>'+
                                     '<td><strong>Contact<strong></td>'+
-                                    '<td>'+filtered[0]['Contact Email']+'</td>'+
+                                    '<td colspan="2">'+filtered[0]['Contact Email']+'</td>'+
                                     '<td><strong>Details<strong></td>'+
-                                    '<td>'+filtered[0]['Details']+'</td>'+
+                                    '<td colspan="2">'+filtered[0]['Details']+'</td>'+
                             '</tr>'+
                             '</tbody>'+
                         '</table>'+
@@ -996,26 +1020,10 @@ $('#regionSelect').on('change', function(e){
     var filter = filteredCfmData;
     
     select != "all" ? filter = filteredCfmData.filter(function(d){ return d['Region'] == select ; }) : null;
-    // filteredCfmData = getFilteredDataFromSelection();
 
-    // filteredCfmData.forEach(element => {
-    //     countriesISO3Arr.includes(element['ISO3']) ? '' : countriesISO3Arr.push(element['ISO3']);
-    // });
     $('#orgSelect').val('all');
     updateDataTable(filter);
     updatePane(filter, select);
-    // reset others filters
-
-    // updateViz();
-    // zoom to region 
-    // if (select == 'all') {
-    //     mapsvg.transition()
-    //     .duration(750)
-    //     .call(zoom.transform, d3.zoomIdentity);
-    // }
-    // zoomToRegion(select);
-    // reset layers selection to all
-    // $('#all').prop('checked', true);
 
   });
 

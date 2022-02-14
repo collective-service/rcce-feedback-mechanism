1531b8ac520c119d36ffcb0ffc52738126f8322b
Vincent Turmine <vincent.turmine@gmail.com>
2022-02-15 00:20:42 +0100

    Update bundle.js

--- js/bundle.js
+++ js/bundle.js
@@ -394,7 +394,12 @@ function getEmergenciesTagged(item){
     var formattedEmergencies = "";
     items.forEach(element => {
         var className = "tag-";
-       
+        if (element == "COVID-19") {
+            className += "COVID-19";
+        } else {
+            var cat = getEmergencyCategory(element);
+            cat != "Epidemics" ? className += "other" : className += cat;
+        }
         formattedEmergencies +='<label class="alert alert-emergency '+className+'">'+element+'</label>';
     });
     return formattedEmergencies;
